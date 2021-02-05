542a6599f5e468eb398572a03ac63f602b52e999
MarkStandriichuk <st.mark.o1994@gmail.com>
2021-02-05 14:51:26 +0200

    modified

---
+++ frontend/src/components/Blog.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Blog extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    this.getPosts();
+  }
+
+  getPosts() {
+    axios
+      .get('http://127.0.0.1:8000/api/v1/')
+      .then(res => {
+        console.log(res);
+        this.setState({ posts: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>DRF + React</h1>
+        {this.state.posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <span>{post.body}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Blog;
